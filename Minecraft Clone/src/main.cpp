5e40a9e73d8520aafc40f998f9e262b19499c1a9
liam.oliver.stewart@gmail.com <liam.oliver.stewart@gmail.com>
2023-12-09 10:41:06 +0000

    Setup and learned git with visual studio

--- Minecraft Clone/src/main.cpp
+++ Minecraft Clone/src/main.cpp
@@ -1,4 +1,7 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main(void)
 {
@@ -14,6 +17,11 @@ int main(void)
         return -1;
     }
     glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+        std::cout << "OOPS" << std::endl;
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
 
     while (!glfwWindowShouldClose(window))
     {
