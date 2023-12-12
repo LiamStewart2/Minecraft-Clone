323223669c0b1c895cfcf6d0331495c38f5e16f4
liam.oliver.stewart@gmail.com <liam.oliver.stewart@gmail.com>
2023-12-12 00:18:57 +0000

    Shader implementation

--- Minecraft Clone/src/Application.cpp
+++ Minecraft Clone/src/Application.cpp
@@ -8,6 +8,10 @@ Application::Application()
 	}
 	else 
 	{
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 		window = glfwCreateWindow(windowWidth, windowHeight, "Minecraft Clone", NULL, NULL);
 		if (!window)
 		{
@@ -22,6 +26,8 @@ Application::Application()
 			else
 			{
 				std::cout << glGetString(GL_VERSION) << std::endl;
+
+				initTriangle();
 			}
 		}
 	}
@@ -35,15 +41,7 @@ void Application::mainloop()
 {
 	while (!glfwWindowShouldClose(window))
 	{
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.2f, -0.2f);
-		glVertex2f(0.2f, -0.2f);
-		glVertex2f(0.0f, 0.2f);
-		glEnd();
-
-		glfwSwapBuffers(window);
+		draw();
 
 		glfwPollEvents();
 	}
@@ -51,7 +49,100 @@ void Application::mainloop()
 	terminate();
 }
 
+void Application::initTriangle()
+{
+	/*
+	glGenBuffers(1, &VBO);
+	//binds the buffer we are working with
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	//bind the vertices to the ARRAY_BUFFER
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	*/
+
+	//store the shader source code
+	//the ID of the shader
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	//load the shader source code into the vertex shader ID
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	//compile the shader
+	glCompileShader(vertexShader);
+
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "shader load error :: vertex shader\n" << infoLog << std::endl;
+	}
+
+
+	//Fragment Shader
+	//the ID of the shader
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	//load the shader source code into the vertex shader ID
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	//compile the shader
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "shader load error :: fragment shader\n" << infoLog << std::endl;
+	}
+
+
+	// Shader Program;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "shader link error\n" << infoLog << std::endl;
+	}
+	/*
+	glUseProgram(shaderProgram);
+	*/
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	/* NEXT BIT */
+
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindVertexArray(0);
+}
+
+void Application::draw()
+{
+	glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glUseProgram(shaderProgram);
+	glBindVertexArray(VAO);
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glfwSwapBuffers(window);
+}
+
 void Application::terminate()
 {
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
+
 	glfwTerminate();
 }
--- Minecraft Clone/src/Application.h
+++ Minecraft Clone/src/Application.h
@@ -17,6 +17,31 @@ private:
 
 	int windowWidth = 720; int windowHeight = 480;
 
+	float vertices[9] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	};
+
+	const char* fragmentShaderSource = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"}\0";
+	const char* vertexShaderSource = "#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\0";
+
+	unsigned int shaderProgram;
+	unsigned int VBO; // vertx buffer object
+	unsigned int VAO; // vertex array object
+
+	void initTriangle();
+	void draw();
 	void terminate();
 };
 
